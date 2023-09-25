63c00f146581c6b7604e6e8ecba1dafd5c0fefe6
Kasolitaire <90893226+Kasolitaire@users.noreply.github.com>
2023-09-25 18:46:56 +0300

    Added original project code

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,79 @@
-import { invoke } from "@tauri-apps/api/tauri";
+import { nanoid } from 'nanoid'
+import type { Task } from 'types/task';
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
 
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
+let taskList: Task[] = [];
+
+const unorderedList = document.querySelector<HTMLUListElement>("#unordered-list")
+const form = document.querySelector<HTMLFormElement>("#new-task-form")
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+const clearButton = document.querySelector<HTMLButtonElement>('#clear-button')
+clearButton?.addEventListener('click', () => {
+    if(confirm('Are you sure you want to clear all tasks, this process cannot be undone')){
+        clearTasksFromLocalStorage();
+        taskList = [];
+    }
+    while(unorderedList?.hasChildNodes()){
+        unorderedList.removeChild(unorderedList.firstChild as Node);
+    }
+})
+
+loadTasksFromLocalStorage();
+
+const submitTask = (event: Event) => {
+    event.preventDefault();
+    if(input?.value == '' || input == null) return;
+
+    const task: Task = {
+        id: nanoid(5),
+        title: input.value,
+        completed: false
+    }
+    taskList.push(task)
+    addListItem(task);
+    saveTasksToLocalStorage(taskList);
+    
+}
+form?.addEventListener('submit', submitTask)
+
+function addListItem(task: Task){
+    const taskElement = document.createElement('li') as HTMLLIElement;
+    taskElement.innerHTML = task.title;
+
+    const deleteButton = document.createElement('button') as HTMLButtonElement;
+    deleteButton.addEventListener('click', (event: Event)=>{
+        deleteButton.parentElement?.remove();
+        removeTaskById(task, taskList);
+    })
+
+    deleteButton.innerHTML = 'Delete Task';
+    
+    const checkbox = document.createElement('input') as HTMLInputElement;
+    checkbox.type = 'checkbox'
+    checkbox.addEventListener('change', (event: Event)=>{
+        task.completed = !task.completed;
+        saveTasksToLocalStorage(taskList);
+    })
+    taskElement.appendChild(checkbox);
+    taskElement.appendChild(deleteButton);
+    
+    unorderedList?.appendChild(taskElement);
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
+function removeTaskById(taskArg: Task, taskListRef: Task[]){
+    taskListRef = taskListRef.filter((task) => task.id != taskArg.id)
+    saveTasksToLocalStorage(taskListRef);
+}
+function saveTasksToLocalStorage(taskList: Task[]){
+    localStorage.setItem('taskListLocalStorage', JSON.stringify(taskList));
+}
+function loadTasksFromLocalStorage(){
+    const taskListStringForm: string | null = localStorage.getItem('taskListLocalStorage')
+    if(taskListStringForm != null) taskList = JSON.parse(taskListStringForm) as Task[];
+    taskList.forEach((task: Task) => addListItem(task))
+}
+
+function clearTasksFromLocalStorage(){
+    localStorage.clear()
+}
+    
