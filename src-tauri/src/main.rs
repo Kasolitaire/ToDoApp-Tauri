e2894c6b231790821c76670935bd3844577cba15
Kasolitaire <90893226+Kasolitaire@users.noreply.github.com>
2023-09-27 02:51:47 +0300

    finally can choose directory to export to

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,13 +8,8 @@ use log::debug;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn create_export_file(directory: String, stringify_data: String){
-    let mut path = PathBuf::new();
-    debug!("{}", path.exists());
-    path.push(directory);
-    
-
-    //let mut file = File::create(path).expect("file not created");
-    //file.write_all(stringify_data.as_bytes()).expect("failed to write to file");
+    let mut file = File::create(directory + r"\test.txt").expect("file not created");
+    file.write_all(stringify_data.as_bytes()).expect("failed to write to file");
 }
 
 fn main() {
