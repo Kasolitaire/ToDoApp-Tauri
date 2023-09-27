a0903844a004df68b965ffce7cc3dae79c80a2da
Kasolitaire <90893226+Kasolitaire@users.noreply.github.com>
2023-09-28 00:20:33 +0300

    Implemented file read command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use std::fs::File;
 use std::io::prelude::*;
-use std::path::PathBuf;
 use log::debug;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -12,10 +11,18 @@ fn create_export_file(directory: String, stringify_data: String){
     file.write_all(stringify_data.as_bytes()).expect("failed to write to file");
 }
 
+#[tauri::command]
+fn read_export_file(file_path: String) -> String{
+    let mut file = File::open(file_path).expect("could not find file");
+    let mut stringify_data = String::new();
+    file.read_to_string(&mut stringify_data).expect("failed to read file");
+    return stringify_data;
+}
+
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_log::Builder::default().build())
-        .invoke_handler(tauri::generate_handler![create_export_file])
+        .invoke_handler(tauri::generate_handler![create_export_file, read_export_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
